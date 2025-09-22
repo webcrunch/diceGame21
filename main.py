d8e58227826f2ed8a5892040bee15ee525bd0fe1
jarl <jarllindquist@gmail.com>
2025-09-22 11:49:37 +0200

    create a function for the dealer that reroll until the score is 17 and up.

--- main.py
+++ main.py
@@ -32,4 +32,21 @@ def player_turn():
             print("Ogiltigt val. Välj 'rulla' eller 'stanna'.")
 
 
-player_turn()
+def dealer_turn():
+    dealer_score = 0
+    print("\nDealern spelar.")
+
+    while dealer_score < 17:
+        dice_roll = roll_dice()
+        dealer_score += dice_roll
+        print(
+            f"Dealern slog en {dice_roll}. Dealerns totala poäng är nu {dealer_score}."
+        )
+
+    if dealer_score > 21:
+        print("Dealerns poäng är över 21! Dealern förlorar.")
+
+    return dealer_score
+
+
+print(dealer_turn())
